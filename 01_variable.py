eea88f8f4d1ae6ec0d59e48f30e175e9f70ca3be
akshaybirthlia <akshaybirtlia@gmail.com>
2025-12-14 16:17:25 +0530

    variable file

---
+++ 01_variable.py
@@ -0,0 +1,6 @@
+number1 = 10; number2 = 30; sum = 0
+name = "Akshay Kumar"; name1 = 'Amitabh Bachchan'
+sum = number1 + number2
+print("The Sum is :", sum)
+print("The Name is :", name)
+print("The Name1 is :", name1)
